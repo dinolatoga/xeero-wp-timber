db232230cd5533c185900e12a8b5e7c999693dd3
Dino Latoga <dinolatoga@gmail.com>
2014-09-25 00:54:58 +0800

    Update theme constants

--- includes/constants.php
+++ includes/constants.php
@@ -9,6 +9,6 @@
  */
 define('THEME_ASSET_VERSION', '0.1.0');
 define( 'THEME_URI', get_stylesheet_directory_uri() );
-define( 'THEME_IMAGES', THEME_URI . '/images' );
-define( 'THEME_CSS', THEME_URI . '/css' );
-define( 'THEME_JS', THEME_URI . '/js' );
+define( 'THEME_IMAGES', THEME_URI . '/assets/images' );
+define( 'THEME_CSS', THEME_URI . '/assets/css' );
+define( 'THEME_JS', THEME_URI . '/assets/js' );
