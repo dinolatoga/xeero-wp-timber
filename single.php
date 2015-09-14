84faca9be72161a15c36a4a69daba1b772a46583
Dino Latoga <dinolatoga@gmail.com>
2015-09-14 14:29:30 +0800

    Fix indentation, no code changes

--- single.php
+++ single.php
@@ -6,13 +6,13 @@
  * @since	2015.08.25
  */
 
- $context = Timber::get_context();
- $post = Timber::query_post();
- $context['post'] = $post;
- $context['comment_form'] = TimberHelper::get_comment_form();
+$context = Timber::get_context();
+$post = Timber::query_post();
+$context['post'] = $post;
+$context['comment_form'] = TimberHelper::get_comment_form();
 
- if ( post_password_required( $post->ID ) ) {
- 	Timber::render( 'single-password.twig', $context );
- } else {
- 	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
- }
+if ( post_password_required( $post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
+} else {
+	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
+}
